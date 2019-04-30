25a3d8d90aeb8f7ffeb99eaf860c0c22b7d3a98a
hdoupe <henrymdoupe@gmail.com>
2019-04-30 11:24:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,13 @@ setuptools.setup(
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/comp-org/Comp-Developer-Kit",
+    url="https://github.com/comp-org/COMP-Developer-Toolkit",
     packages=setuptools.find_packages(),
     install_requires=["paramtools>=0.5.1"],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
