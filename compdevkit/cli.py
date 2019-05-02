c93d89111f69278fe63da97174c35d536ad838e6
hdoupe <henrymdoupe@gmail.com>
2019-05-02 09:41:25 -0400

    Write python package so that sys.path hacking is not needed

--- compdevkit/cli.py
+++ compdevkit/cli.py
@@ -1,16 +1,42 @@
 from pathlib import Path
 
+functionstemplate = "# Write or import your COMP functions here."
+
+testfunctionstemplate = """from compdevkit import FunctionsTest
+
+from compconfig import functions
+
+
+def test_functions():
+    # test your functions with FunctionsTest here
+    pass
+"""
+
+setuptemplate = "# setup.py helps py.test find your tests in comp/tests/test_functions.py"
+
+
+def write_template(path, template):
+    with open(path, "w") as f:
+        f.write(template)
+
 def init():
-    comp = Path.cwd() / "comp"
+    comptoplevel = Path.cwd() / "compconfig"
+    comptoplevel.mkdir()
+    (comptoplevel / "setup.py").touch()
+    write_template(comptoplevel / "setup.py", setuptemplate)
+
+    comp = comptoplevel / "compconfig"
     comp.mkdir()
-    compinit = comp / "__init__.py"
-    compinit.touch()
-    compfunctions = comp / "functions.py"
-    compfunctions.touch()
+
+    (comp / "__init__.py").touch()
+
+    (comp / "functions.py").touch()
+    write_template(comp / "functions.py", functionstemplate)
 
     test = comp / "tests"
     test.mkdir()
-    testinit = test / "__init__.py"
-    testinit.touch()
-    testfunctions  = test / "test_functions.py"
-    testfunctions.touch()
+
+    (test / "__init__.py").touch()
+
+    (test / "test_functions.py").touch()
+    write_template(test / "test_functions.py", testfunctionstemplate)
