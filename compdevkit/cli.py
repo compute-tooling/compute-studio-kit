39c5f54c06a40f662e9d3d0564ebdec750fa14ab
hdoupe <henrymdoupe@gmail.com>
2019-05-02 15:10:04 -0400

    Write setup.py function

--- compdevkit/cli.py
+++ compdevkit/cli.py
@@ -12,7 +12,24 @@ def test_functions():
     pass
 """
 
-setuptemplate = "# setup.py helps py.test find your tests in comp/tests/test_functions.py"
+setuptemplate = """\"\"\"
+setup.py is used to build a light-weight python package around your
+COMP code. Add a MANIFEST.in file to specify data files that should
+be included with this package. Read more here:
+https://docs.python.org/3.8/distutils/sourcedist.html#specifying-the-files-to-distribute
+\"\"\"
+
+import setuptools
+import os
+
+setuptools.setup(
+    name="compconfig",
+    description="COMP configuration files.",
+    url="https://github.com/comp-org/COMP-Developer-Toolkit",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
+"""
 
 
 def write_template(path, template):
