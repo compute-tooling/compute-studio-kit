cecd34b63e817103558965aaef612845f1ca6d86
hdoupe <henrymdoupe@gmail.com>
2019-09-11 16:35:03 -0400

    Add cs-config directory for example

---
+++ cs-config/cs_config/tests/test_functions.py
@@ -0,0 +1,12 @@
+from cs_kit import CoreTestFunctions
+
+from cs_config import functions
+
+
+class TestFunctions1(CoreTestFunctions):
+    get_version = functions.get_version
+    validate_inputs = functions.validate_inputs
+    run_model = functions.run_model
+    ok_adjustment = {} # your valid inputs here
+    bad_adjustment = {} # your invalid
+
