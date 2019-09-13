55c097092739c62c77d73faf7005e5e511bd6eab
hdoupe <henrymdoupe@gmail.com>
2019-09-13 11:18:09 -0400

    Include get_inputs in template

--- cs-config/cs_config/tests/test_functions.py
+++ cs-config/cs_config/tests/test_functions.py
@@ -5,6 +5,7 @@ from cs_config import functions
 
 class TestFunctions1(CoreTestFunctions):
     get_version = functions.get_version
+    get_inputs = functions.get_inputs
     validate_inputs = functions.validate_inputs
     run_model = functions.run_model
     ok_adjustment = {}  # your valid inputs here
