1cab65327e4cf1c3e6e06638ba67942a686426d5
hdoupe <henrymdoupe@gmail.com>
2019-09-19 10:40:40 -0400

    Bump version to 1.8.1

--- cs_kit/__init__.py
+++ cs_kit/__init__.py
@@ -2,7 +2,7 @@ from .exceptions import CSKitError, SerializationError
 from .schemas import Parameters, ErrorsWarnings
 from .validate import CoreTestFunctions
 
-__version__ = "1.8.0"
+__version__ = "1.8.1"
 
 
 __all__ = [
