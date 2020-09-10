723d4261536dbce31b925867ddf1a7a69c134e9c
Hank Doupe <henrymdoupe@gmail.com>
2020-09-10 14:58:23 -0400

    Add filespec tests and bump version

--- cs_kit/__init__.py
+++ cs_kit/__init__.py
@@ -2,19 +2,21 @@ from .api import ComputeStudio
 from .exceptions import CSKitException, CSKitError, SerializationError, APIException
 from .schemas import Parameters, ErrorsWarnings
 from .validate import CoreTestFunctions
+from .filespec import CSFileSystem
 
-__version__ = "1.11.0"
+__version__ = "1.11.1"
 
 
 __all__ = [
-    CSKitError,
-    SerializationError,
-    Parameters,
-    ErrorsWarnings,
-    CoreTestFunctions,
-    ComputeStudio,
-    CSKitException,
-    CSKitError,
-    SerializationError,
-    APIException,
+    "CSKitError",
+    "SerializationError",
+    "Parameters",
+    "ErrorsWarnings",
+    "CoreTestFunctions",
+    "ComputeStudio",
+    "CSKitException",
+    "CSKitError",
+    "SerializationError",
+    "APIException",
+    "CSFileSystem",
 ]
