ed3feae8e7cd68aa03f01fbecf09ba4c7634908a
Hank Doupe <henrymdoupe@gmail.com>
2021-03-22 20:22:22 -0400

    Bump version

--- cs_kit/__init__.py
+++ cs_kit/__init__.py
@@ -4,7 +4,7 @@ from .schemas import Parameters, ErrorsWarnings
 from .validate import CoreTestFunctions
 from .filespec import CSFileSystem
 
-__version__ = "1.16.2"
+__version__ = "1.16.3"
 
 
 __all__ = [
